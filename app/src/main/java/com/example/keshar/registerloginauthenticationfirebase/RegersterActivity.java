8223fb9964221bb53989fc78894fe65f59e7d237
Keshar <paudelkeshar2055@gmail.com>
2019-04-10 07:00:00 +0545

    Register activity is added in app

---
+++ app/src/main/java/com/example/keshar/registerloginauthenticationfirebase/RegersterActivity.java
@@ -0,0 +1,13 @@
+package com.example.keshar.registerloginauthenticationfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegersterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regerster);
+    }
+}
